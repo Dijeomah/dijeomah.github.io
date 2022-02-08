8cf5488dfa39626434746766212855c7b89dbe0f
dijeomah <98405382+zainabsadeeqsule-iko@users.noreply.github.com>
2022-02-08 11:28:12 +0100

    Added mail

---
+++ form.php
@@ -0,0 +1,11 @@
+<?php
+$ToEmail = 'success.drame@gmail.com';
+$EmailSubject = $_POST["subject"];
+$mailheader = "From: ".$_POST["email"]."\r\n";
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n";
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$MESSAGE_BODY = "Name: ".$_POST["name"]."";
+$MESSAGE_BODY .= "Email: ".$_POST["email"]."";
+$MESSAGE_BODY .= "Message: ".nl2br($_POST["text"])."";
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
+?>
