09cbb87d19bba2938bb6fbfa97c857f10b23dd52
Drame Success <d.ije95@gmail.com>
2022-04-29 12:06:03 +0100

    -new dp

--- form.php
+++ form.php
@@ -4,8 +4,8 @@ $EmailSubject = $_POST["subject"];
 $mailheader = "From: ".$_POST["email"]."\r\n";
 $mailheader .= "Reply-To: ".$_POST["email"]."\r\n";
 $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n";
-$MESSAGE_BODY = "Name: ".$_POST["name"]."";
-$MESSAGE_BODY .= "Email: ".$_POST["email"]."";
-$MESSAGE_BODY .= "Message: ".nl2br($_POST["text"])."";
+$MESSAGE_BODY = "Name: ".$_POST["name"]."\n";
+$MESSAGE_BODY .= "Email: ".$_POST["email"]."\n";
+$MESSAGE_BODY .= "Message: ".nl2br($_POST["text"])."\n";
 mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
 ?>
